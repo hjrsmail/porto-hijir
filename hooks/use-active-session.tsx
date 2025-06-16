4fd6e144eca24f4c535175a20c7453daa56d2416
hjrsmail <hijir512@gmail.com>
2025-06-16 22:06:28 +0800

    add hooks

---
+++ hooks/use-active-session.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+
+export const useActiveSection = (sectionIds: string[]) => {
+    const [activeId, setActiveId] = useState<string>("");
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        setActiveId(entry.target.id);
+                    }
+                });
+            },
+            { threshold: 0.5 } // Bisa kamu ubah sesuai kebutuhan
+        );
+
+        sectionIds.forEach(id => {
+            const el = document.getElementById(id);
+            if (el) observer.observe(el);
+        });
+
+        return () => observer.disconnect();
+    }, [sectionIds]);
+
+    return activeId;
+};
