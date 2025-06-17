269b4001e379a1dbbb88c80593c7b11bf5194589
hjrsmail <hijir512@gmail.com>
2025-06-17 13:36:40 +0800

    modified and update routes api

---
+++ app/api/projects/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const res = await fetch(`${process.env.APP_URL}/projects`, {
+    headers: {
+      "X-API-KEY": process.env.API_KEY as string,
+      Accept: "application/json",
+    },
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    return NextResponse.json(
+      { error: "Gagal mengambil data" },
+      { status: 500 }
+    );
+  }
+
+  const json = await res.json();
+  return NextResponse.json(json);
+}
--- server/projects.ts
+++ server/projects.ts
@@ -12,18 +12,9 @@ export interface Project {
 }
 
 export const get = async () => {
-  const res = await fetch(`${process.env.APP_URL}/projects`, {
-    headers: {
-      "X-API-KEY": process.env.API_KEY as string,
-      Accept: "application/json",
-    },
-    cache: "no-store",
-  });
-
-  if (!res.ok) {
-    throw new Error("Gagal mengambil data project");
-  }
-
+  const res = await fetch(`/api/projects`);
+  if (!res.ok) throw new Error("Gagal mengambil data project");
   const json = await res.json();
   return json.data;
 };
+
