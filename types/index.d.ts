f3ade4f9626bbc4655f24b109be780a6594448c1
Hijir Ismail <hijir512@gmail.com>
2025-08-19 21:00:15 +0800

    refactor: standardize property names in Project interface for consistency

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,16 +1,21 @@
-import { LucideIcon } from 'lucide-react';
-import type { Config } from 'ziggy-js';
+import { LucideIcon } from "lucide-react";
+import type { Config } from "ziggy-js";
 
 export interface Project {
   id: number;
   image: string;
   title: string;
   description: string;
-  date: string;
-  url?: string;
-  github?: string;
+  created_at: string;
+  demo_url?: string;
+  github_url?: string;
   author: string;
 }
 
-
-
+export interface Certificate {
+  id: number;
+  title: string;
+  image: string;
+  // description: string;
+  publisher: string;
+}
